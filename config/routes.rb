645ead1274a6b8906a7d10313a53405f4aded669
yu-web <54567280+yu-web@users.noreply.github.com>
2019-11-09 18:36:02 +0900

    Static page (#3)
    * made static_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'staticpages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+  root "staticpages#home"
+end 
